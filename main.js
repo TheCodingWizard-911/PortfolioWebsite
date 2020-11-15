a6f6fd40e0926e2741d34d9c56e85234b2d536d9
Ganesh-911 <ganeshs.gitte@gmail.com>
2020-11-15 22:31:11 +0530

    changing murger menu icon

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const selectElement = (s) => document.querySelector(s);
+
 const navLinks = document.querySelectorAll('.nav-link');
 
-selectElement('.burger-menu-icon').addEventListener('click', () => {
+const menuIcon = document.getElementById('menu');
+
+selectElement('.menu-icon').addEventListener('click', () => {
   selectElement('.nav-list').classList.toggle('active');
-  selectElement('.burger-menu-icon').classList.toggle('toggle');
+
+  if (menuIcon.className == 'fas fa-bars fa-3x') {
+    menuIcon.className = 'fas fa-times fa-3x';
+  } else if (menuIcon.className == 'fas fa-times fa-3x') {
+    menuIcon.className = 'fas fa-bars fa-3x';
+  }
 
   navLinks.forEach((link, index) => {
     if (link.style.animation) {
@@ -20,8 +28,11 @@ selectElement('.burger-menu-icon').addEventListener('click', () => {
 navLinks.forEach((link) => {
   link.addEventListener('click', () => {
     selectElement('.nav-list').classList.toggle('active');
-    selectElement('.burger-menu-icon').classList.toggle('toggle');
-
+    if (menuIcon.className == 'fas fa-bars fa-3x') {
+      menuIcon.className = 'fas fa-times fa-3x';
+    } else if (menuIcon.className == 'fas fa-times fa-3x') {
+      menuIcon.className = 'fas fa-bars fa-3x';
+    }
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
